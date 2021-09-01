472948c6f0fff169605c1a1ebdbba216c294ea8d
MiguelEWV <miguelwellspay1@gmail.com>
2021-09-01 14:18:48 -0300

    Cambio De Nombre De La ActividadInicial.kt

--- app/src/main/java/com/miguelewv/imccalculator/ActividadInicial.kt
+++ app/src/main/java/com/miguelewv/imccalculator/ActividadInicial.kt
@@ -3,7 +3,7 @@ package com.miguelewv.imccalculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ActividadInicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
