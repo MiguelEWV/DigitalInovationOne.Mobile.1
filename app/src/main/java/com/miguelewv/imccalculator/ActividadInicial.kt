865a66dd43da4922d3a74da2bfd877c2db3cd06b
MiguelEWV <miguelwellspay1@gmail.com>
2021-09-02 11:36:06 -0300

    Todo layout 50%

--- app/src/main/java/com/miguelewv/imccalculator/ActividadInicial.kt
+++ app/src/main/java/com/miguelewv/imccalculator/ActividadInicial.kt
@@ -6,6 +6,11 @@ import android.os.Bundle
 class ActividadInicial : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.actividadinicial)
+
     }
+
+
+
+
 }
